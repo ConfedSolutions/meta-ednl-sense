a222620024cf2280a02fa0a23549bd80c71573b3
N. van IJzendoorn <nick.vanijzendoorn@confed.eu>
2026-03-11 11:55:02 +0100

    disable the 2nd antenna input and display modem information

--- recipes-sense/quectel-modem/files/modem_initialize.sh
+++ recipes-sense/quectel-modem/files/modem_initialize.sh
@@ -5,8 +5,27 @@ while ! modem-send -v "ATE0"; do
 	echo "retrying to disable command echoing";
 done
 
+# disable the second antenna input
+modem-send "AT+QCFG=\"divctl\",0"
+
 # normalize IPv6 address printing
 modem-send "AT+CGPIAF=1,1,1,0"
+
+# display the identifiers
+echo -n "[+] Module information"
+modem-send "ATI"
+
+echo -n "[+] ICCID:"
+modem-send "AT+QCCID"
+
+echo -n "[+] IMSI:"
+modem-send "AT+CIMI"
+
+echo -n "[+] IMEI:"
+modem-send "AT+GSN"
+
+echo -n "[+] Firmware Version:"
+modem-send "AT+QGMR"
 
 # check if the modem is configured for cdc_ether mode
 echo -n "Check if modem in cdc_ether mode: "
